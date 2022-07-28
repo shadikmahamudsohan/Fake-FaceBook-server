1372dcd57520f002bbd9761651502719731ae302
shadik <shadikmahamudsohan@gmail.com>
2022-07-29 00:04:02 +0600

    getting single user

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+const ObjectId = require('mongodb').ObjectId;
 
 //middleware
 app.use(cors());
@@ -20,23 +21,24 @@ async function run() {
         // all collection here
         const postCollection = client.db("Posts").collection("post");
         const userCollection = client.db("Users").collection("user");
-
         // post methods
         app.get('/post', async (req, res) => {
             const posts = await postCollection.find({}).toArray();
             res.send(posts);
         });
+        //create post
         app.post('/post', async (req, res) => {
             // console.log(req.body);
             const data = req.body;
             const post = await postCollection.insertOne(data);
             res.send(post);
         });
-
+        //delete post
         app.delete('/post/:id', async (req, res) => {
             const post = await postCollection.deleteOne({ _id: req.params.id });
             res.send(post);
         });
+        //update post
         app.put('/post/:id', async (req, res) => {
             const post = await postCollection.updateOne({ _id: req.params.id }, { $set: req.body });
             res.send(post);
@@ -46,14 +48,23 @@ async function run() {
             const users = await userCollection.find({}).toArray();
             res.send(users);
         });
+        //get single user
+        app.get('/user/:id', async (req, res) => {
+            const user = await userCollection.findOne({ _id: ObjectId(req.params.id) });
+            console.log(user);
+            res.send(user);
+        });
+        //create user
         app.post('/user', async (req, res) => {
             const user = await userCollection.insertOne(req.body);
             res.send(user);
         });
+        //delete user
         app.delete('/user/:id', async (req, res) => {
             const user = await userCollection.deleteOne({ _id: req.params.id });
             res.send(user);
         });
+        //update user
         app.put('/user/:id', async (req, res) => {
             const user = await userCollection.updateOne({ email: req.params.id }, { $set: req.body });
             res.send(user);
